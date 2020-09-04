c0fe8686ac186a3ecd1e6b3351a55f53415b1d05
Austen Zeh <developerdawg@gmail.com>
2020-09-03 20:22:30 -0400

    fix: Updated the example app
    Fixed the extry point of the exqmple app

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './example/src/App';
+import {name as appName} from './example/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
