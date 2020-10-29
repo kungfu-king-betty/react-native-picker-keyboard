0f8fa8c08594472b9c8fc4df9a4c6b088062fc5a
Austen Zeh <developerdawg@gmail.com>
2020-10-29 14:01:00 -0400

    0.0.1-l

--- ios/RNPickerKeyboard.m
+++ ios/RNPickerKeyboard.m
@@ -6,6 +6,7 @@
 {
     self = [super initWithFrame:frame];
     UITextField* textF = [[UITextField alloc] initWithFrame:frame];
+    textF.backgroundColor = [UIColor colorWithRed:0.2 green:0.9 blue:0.5 alpha:0.3];
 
     [self addSubview:textF];
 
